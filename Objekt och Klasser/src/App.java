5de9d5119b5d05513ca39bceb20b188810f2a4c7
Love Rumar Karlquist <lovebandy@hotmail.se>
2023-11-06 13:52:32 +0100

    Första guide videon

---
+++ Objekt och Klasser/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Player player1 = new Player();
+        player1.playerName = "Love";
+        System.out.println(player1.playerName);
+        //player1.levelUp();
+
+        Player player2 = new Player();
+        player2.playerName = "Maria";
+        System.out.println(player2.playerName);
+    }
+}
---
+++ Objekt och Klasser/src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    String playerName;
+    int maxHealth = 100;
+    int level = 1;
+
+    public void levelUp() {
+        System.out.println("Player leveled up");
+    }
+}
