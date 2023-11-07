c02a5fbf73c617baeb2fd40f9ab40c2bc46a4b84
Love Rumar Karlquist <lovebandy@hotmail.se>
2023-11-07 10:05:56 +0100

    packages

---
+++ Packages/src/App.java
@@ -0,0 +1,15 @@
+//import java.util.Scanner;
+
+import java.lang.*;
+
+import player.Player; //player är mapp namnet, Player är class namnet
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Player player = new Player();
+
+        //Scanner sc = new Scanner(System.in);
+
+        System.out.println("hej hej");
+    }
+}
---
+++ Packages/src/player/Player.java
@@ -0,0 +1,7 @@
+package player;
+
+//import com.loverumar.calculator; //importera från internet. Det efter com. och innan .calculator måste vara unikt
+
+public class Player {
+    String playerName = "Love";
+}
